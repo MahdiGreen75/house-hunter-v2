4036d0ed46ed307da451b0f870e5038aa9369959
Ricky Ponting <qarimahdi217@gmail.com>
2024-01-24 11:28:23 +0600

    test.

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,17 @@ function App() {
 
   return (
     <>
-      <h1>The reason why are seeing this page whole empty.</h1>
-      <p>Usually , i do the backend side first. i ful filled the backend part. but, I was unable to complete the client side.</p>
-      <h1> the clinet is not ready yet.</h1>
-      <a rel="noreferrer" href='https://github.com/MahdiGreen75/House-hunter-client'>here is the server side github link.</a>
-      <a rel="noreferrer" href='https://house-hunter-navy.vercel.app/'>here is the server live link.</a>
-      <a rel="noreferrer" href='https://house-hunter-navy.vercel.app/api/v1/seed'>here is the server test link.</a>
+      <h1 className='text-3xl font-bold'>The reason why are seeing this page whole empty.</h1>
+      <p>Usually , i do the backend side first. i ful filled the backend part. but, I was unable to complete the client side in time.</p>
+      <h1> the client is not ready yet.</h1>
+      <a className='text-blue-400 underline' rel="noreferrer" href='https://github.com/MahdiGreen75/House-hunter-client'>here is the server side github link.</a>
+      <br />
+      <a  className='text-blue-400 underline' rel="noreferrer" href='https://house-hunter-navy.vercel.app/'>here is the server live link.</a>
+      <br />
+      <a  className='text-blue-400 underline' rel="noreferrer" href='https://house-hunter-navy.vercel.app/api/v1/seed'>here is the server test link.</a>
+      <div className='flex justify-center items-center' >
+        <img className='w-48 mt-6' src="https://hips.hearstapps.com/goodhousekeeping-uk/main/embedded/29905/child-crying.jpg?resize=980:*" alt="" />
+      </div>
     </>
   )
 }
