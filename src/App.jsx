1f60336ff1cf83ad24fbda81af6ccda738c4f4b8
Ricky Ponting <qarimahdi217@gmail.com>
2024-01-24 11:15:24 +0600

    test.

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ function App() {
       <h1>The reason why are seeing this page whole empty.</h1>
       <p>Usually , i do the backend side first. i ful filled the backend part. but, I was unable to complete the client side.</p>
       <h1> the clinet is not ready yet.</h1>
-      <a href='https://github.com/MahdiGreen75/House-hunter-client'>here is the server side github link.</a>
-      <a href='https://house-hunter-navy.vercel.app/'>here is the server live link.</a>
-      <a href='https://house-hunter-navy.vercel.app/api/v1/seed'>here is the server test link.</a>
+      <a rel="noreferrer" href='https://github.com/MahdiGreen75/House-hunter-client'>here is the server side github link.</a>
+      <a rel="noreferrer" href='https://house-hunter-navy.vercel.app/'>here is the server live link.</a>
+      <a rel="noreferrer" href='https://house-hunter-navy.vercel.app/api/v1/seed'>here is the server test link.</a>
     </>
   )
 }
