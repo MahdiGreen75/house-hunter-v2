46403dbdd60a20519546d2f41e1c931c079f1e1e
Ricky Ponting <qarimahdi217@gmail.com>
2024-01-24 11:13:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>The reason why are seeing this page whole empty.</h1>
+      <p>Usually , i do the backend side first. i ful filled the backend part. but, I was unable to complete the client side.</p>
+      <h1> the clinet is not ready yet.</h1>
+      <a href='https://github.com/MahdiGreen75/House-hunter-client'>here is the server side github link.</a>
+      <a href='https://house-hunter-navy.vercel.app/'>here is the server live link.</a>
+      <a href='https://house-hunter-navy.vercel.app/api/v1/seed'>here is the server test link.</a>
+    </>
+  )
+}
+
+export default App
